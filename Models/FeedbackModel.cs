0a3ded6ca2026b220b4a654af7f06ab1b09b6095
白洲未晞 <ShirasuWeixi@163.com>
2026-05-26 02:50:10 +0800

    反馈界面的数据模型

---
+++ Models/FeedbackModel.cs
@@ -0,0 +1,9 @@
+namespace RocoKingdomEggQuery.Models
+{
+    public class FeedbackModel
+    {
+        public string SizeData { get; set; } = string.Empty;
+        public string MassData { get; set; } = string.Empty;
+        public string SpriteName { get; set; } = string.Empty;
+    }
+}
